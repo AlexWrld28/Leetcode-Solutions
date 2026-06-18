cc768acc322b2b7476cd6035fbd1cb9420804947
Alexander Zirilli <142417763+AlexWrld28@users.noreply.github.com>
2026-06-18 14:19:28 -0400

    Time: 164 ms (15.91%), Space: 89.4 MB (48.28%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -6,7 +6,7 @@
 var flat = function (arr, n) {
     const result = [];
 
-    for (i in arr) {
+    for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(arr[i]) && n > 0) {
             const flattened = flat(arr[i], n - 1);
             result.push(...flattened);
